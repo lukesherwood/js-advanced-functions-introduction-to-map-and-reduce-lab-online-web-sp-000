1f894d5fb5d66565286471ec5873edffdac8781b
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-10 20:29:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function reduceToAllTrue (array) {
   return true
 }
 
-function reduceToAllTrue (array) {
+function reduceToAnyTrue (array) {
   for (const element of array) {
     if (element) return true
   }
