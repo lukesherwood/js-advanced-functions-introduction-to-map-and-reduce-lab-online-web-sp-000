0818fa8071fba352d9a7be95c27acce1dece85b4
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-10 20:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(array){
+  let ar = []
+  for (const element of array) {
+  ar.push(element*-1);
+  }
+  return ar
+}
+
+function mapToNoChange(array){
+  return array
+}
+
+function mapToDouble (array){
+  let ar = []
+  for (const element of array) {
+  ar.push(element*2);
+  }
+  return ar
+}
+
+function mapToSquare (array){
+  let ar = []
+  for (const element of array) {
+  ar.push(element*element);
+  }
+  return ar
+}
+
+function reduceToTotal (array, total=0){
+  for (const element of array) {
+  total += element;
+  }
+  return total;
+}
+
+function reduceToAllTrue (array) {
+  for (const element of array) {
+    if (!element) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue (array) {
+  for (const element of array) {
+    if (element) return true
+  }
+  return false
+}
+
+
+
+
